d796e5128a27e85d585857a2ab12ffd6e417482b
TungPV <vantung14.qn@gmail.com>
2021-12-01 10:56:34 +0700

    sua giao dien login admin 2

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -6,13 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{ asset("admin/login_template/images/icons/favicon.ico") }}"/>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset("admin/login_template/vendor/bootstrap/css/bootstrap.min.css") }}">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset("admin/login_template/fonts/font-awesome-4.7.0/css/font-awesome.min.css") }}">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset("admin/login_template/fonts/Linearicons-Free-v1.0.0/icon-font.min.css") }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/fontawesome-free/css/all.min.css") }}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css") }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css") }}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/jqvmap/jqvmap.min.css") }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset("admin/dist/css/adminlte.min.css") }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/daterangepicker/daterangepicker.css") }}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset("admin/plugins/summernote/summernote-bs4.min.css") }}">
+    <!-- Fontawsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset("admin/login_template/css/util.css") }}">
     <link rel="stylesheet" type="text/css" href="{{ asset("admin/login_template/css/main.css") }}">
     <!--===============================================================================================-->
@@ -72,11 +87,38 @@
         </div>
     </div>
 </div>
-
+<script src="{{ asset("admin/plugins/jquery/jquery.min.js") }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ asset("admin/plugins/jquery-ui/jquery-ui.min.js") }}"></script>
 <!--===============================================================================================-->
-<script src="{{ asset("admin/login_template/vendor/jquery/jquery-3.2.1.min.js") }}"></script>
-<script src="{{ asset("admin/login_template/vendor/bootstrap/js/popper.js") }}"></script>
-<script src="{{ asset("admin/login_template/vendor/bootstrap/js/bootstrap.min.js") }}"></script>
+<script src="{{ asset("admin/plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<!-- ChartJS -->
+<script src="{{ asset("admin/plugins/chart.js/Chart.min.js") }}"></script>
+<!-- Sparkline -->
+<script src="{{ asset("admin/plugins/sparklines/sparkline.js") }}"></script>
+<!-- JQVMap -->
+<script src="{{ asset("admin/plugins/jqvmap/jquery.vmap.min.js") }}"></script>
+<script src="{{ asset("admin/plugins/jqvmap/maps/jquery.vmap.usa.js") }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ asset("admin/plugins/jquery-knob/jquery.knob.min.js") }}"></script>
+<!-- daterangepicker -->
+<script src="{{ asset("admin/plugins/moment/moment.min.js") }}"></script>
+<script src="{{ asset("admin/plugins/daterangepicker/daterangepicker.js") }}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{ asset("admin/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}"></script>
+<!-- Summernote -->
+<script src="{{ asset("admin/plugins/summernote/summernote-bs4.min.js") }}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset("admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset("admin/dist/js/adminlte.js") }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset("admin/dist/js/demo.js") }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset("admin/dist/js/pages/dashboard.js") }}"></script>
+<script src="{{ asset("frontend/ckeditor/ckeditor.js") }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
 <!--===============================================================================================-->
 <script src="{{ asset("admin/login_template/js/main.js") }}"></script>
 
