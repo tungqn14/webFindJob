ef9c0ec46540f68ab2c3feae2442034e9a894e02
TungPV <vantung14.qn@gmail.com>
2021-12-03 16:16:14 +0700

    sua api get all company

--- app/Http/Controllers/api/CompanyController.php
+++ app/Http/Controllers/api/CompanyController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Model\Company;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\JWTAuth;
 
 class CompanyController extends Controller
 {
     protected $company;
-    public function __construct()
+    public function __construct(Company $company)
     {
+        $this->company = $company;
         //$this->middleware('jwt', ['except' => ['index']]);
     }
     public function index(){
