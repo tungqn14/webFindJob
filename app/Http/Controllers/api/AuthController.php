93faefebf91b6f10e49b32968ba5ac388e72aa29
TungPV <vantung14.qn@gmail.com>
2021-12-05 15:07:19 +0700

    viet api cap nhat nguoi dung

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -52,12 +52,12 @@ class AuthController extends Controller
             if ($user && Hash::check($request->password, $user->password))
             {
                 if($user->auth_token){
-                  return  response()->json(['success'=>true, 'data'=>['id'=>$user->id,'auth_token'=>$user->auth_token,'name'=>$user->fullName, 'email'=>$user->email,'phone'=>$user->phone,"address"=>$user->address,"desiredMoney"=>$user->desiredMoney,"avatar"=>$user->avatar,"exp"=>$user->exp,"descripYourself"=>$user->descripYourself,"position"=>$user->position,"cv"=>$user->cv,"birthDay"=>$user->birthDay]]) ;
+                  return  response()->json(['success'=>true, 'data'=>['id'=>$user->id,'auth_token'=>$user->auth_token,'fullName'=>$user->fullName, 'email'=>$user->email,'phone'=>$user->phone,"address"=>$user->address,"desiredMoney"=>$user->desiredMoney,"avatar"=>$user->avatar,"exp"=>$user->exp,"descripYourself"=>$user->descripYourself,"position"=>$user->position,"cv"=>$user->cv,"birthDay"=>$user->birthDay]]) ;
                 }
                 $token = self::getToken($request->email, $request->password);
                 $user->auth_token = $token;
                 if($user->save()){
-                    return response()->json(['success' => true, 'data' => ['id'=>$user->id,'auth_token'=>$user->auth_token,'name'=>$user->fullName, 'email'=>$user->email,'phone'=>$user->phone,"address"=>$user->address,"desiredMoney"=>$user->desiredMoney,"avatar"=>$user->avatar,"exp"=>$user->exp,"descripYourself"=>$user->descripYourself,"position"=>$user->position,"cv"=>$user->cv,"birthDay"=>$user->birthDay]]);
+                    return response()->json(['success' => true, 'data' => ['id'=>$user->id,'auth_token'=>$user->auth_token,'fullName'=>$user->fullName, 'email'=>$user->email,'phone'=>$user->phone,"address"=>$user->address,"desiredMoney"=>$user->desiredMoney,"avatar"=>$user->avatar,"exp"=>$user->exp,"descripYourself"=>$user->descripYourself,"position"=>$user->position,"cv"=>$user->cv,"birthDay"=>$user->birthDay]]);
                 }
                 return response()->json(["success"=>false,"data"=>"Đăng nhập thất bại"], 500);
             }
@@ -94,7 +94,7 @@ class AuthController extends Controller
             $payload = [
                 'password' => Hash::make($request->password),
                 'email' => $request->email,
-                'fullName' => $request->name,
+                'fullName' => $request->fullName,
                 'phone' => $request->phone,
                 'user_level' => 2,
                 'auth_token' => '',
@@ -112,7 +112,7 @@ class AuthController extends Controller
                 $user->auth_token = $token; // update user token
 
                 if($user->save()){
-                    return response()->json(['success' => true, 'data' => ['id'=>$user->id,'auth_token'=>$user->auth_token,'name'=>$user->fullName, 'email'=>$user->email,'phone'=>$user->phone,"address"=>$user->address,"desiredMoney"=>$user->desiredMoney,"avatar"=>$user->avatar,"exp"=>$user->exp,"descripYourself"=>$user->descripYourself,"position"=>$user->position,"cv"=>$user->cv,"birthDay"=>$user->birthDay]]);
+                    return response()->json(['success' => true, 'data' => ['id'=>$user->id,'auth_token'=>$user->auth_token,'fullName'=>$user->fullName,'email'=>$user->email,'phone'=>$user->phone,"address"=>$user->address,"desiredMoney"=>$user->desiredMoney,"avatar"=>$user->avatar,"exp"=>$user->exp,"descripYourself"=>$user->descripYourself,"position"=>$user->position,"cv"=>$user->cv,"birthDay"=>$user->birthDay]]);
                 }
                 return response()->json(['success' => false, 'data'=>"Token tạo ko thành công hãy quay lại đăng nhập"]);
             } else
@@ -123,11 +123,7 @@ class AuthController extends Controller
     }
 
     public function logOut(Request $request){
-        $validator = Validator::make($request->all(),[
-            'token' => 'required',
-        ],[
-            "token.required"=>"Token không được để trống",
-        ]);
+
        $user = User::where("auth_token",$request->token)->get()->first();
        if($user){
            $user->auth_token = "";
@@ -137,5 +133,59 @@ class AuthController extends Controller
            return response()->json(['message' => 'Đăng xuất thất bại',"status"=>500]);
        }
         return response()->json(['message' => 'Đăng xuất thất bại',"status"=>500]);
+    }
+    public function updateUser(Request $request){
+        $user = User::where("auth_token",$request->token)->get()->first();
+        $validator = Validator::make($request->all(),[
+            'email' =>'required|unique:users,email,'.$user->id,
+            'fullName' => 'required',
+            'birthDay' => 'required',
+            'address' => 'required',
+            'desiredMoney' => 'required',
+            'avatar' => 'required',
+            'exp' => 'required',
+            'descripYourself' => 'required',
+            'position' => 'required',
+            'cv' => 'required',
+            'phone' => 'required',
+        ],[
+            "fullName.required"=>"Tên đầy đủ không được để trống",
+            "email.required"=>"Email không được để trống",
+            "email.unique"=>"Email đã tồn tại, vui lòng nhập email khác",
+            "birthDay.required"=>"Ngày tháng năm sinh không được để trống",
+            "address.required"=>"Địa chỉ không được để trống",
+            "desiredMoney.required"=>"Tiền lương mong muốn không được để trống",
+            "avatar.required"=>"Link ảnh không được để trống",
+            "exp.required"=>"Kinh nghiệm không được để trống",
+            "descripYourself.required"=>"Mô tả bản thân không được để trống",
+            "position.required"=>"Vị trí hiện tại không được để trống",
+            "cv.required"=>"Link CV cá nhân ko được để trống không được để trống",
+            "phone.required"=>"Link CV cá nhân ko được để trống không được để trống",
+
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                "message"=>$validator->errors(),
+                "status"=> 400
+            ]);
+        }else{
+            $user->email = $request->email;
+            $user->fullName = $request->fullName;
+            $user->birthDay = $request->birthDay;
+            $user->address = $request->address;
+            $user->desiredMoney = $request->desiredMoney;
+            $user->exp = $request->exp;
+            $user->position = $request->position;
+            $user->descripYourself = $request->descripYourself;
+            $user->phone = $request->phone;
+            $user->cv = $request->cv;
+            $user->avatar = $request->avatar;
+            if($user->save()){
+                return response()->json(['message' => 'Cập nhật thông tin thành công',"status"=>200]);
+            }
+            return response()->json(['message' => 'Cập nhật thông tin thât bại',"status"=>200]);
+        }
+
+        return response()->json(['message' => 'Cập nhật thông tin thất bại',"status"=>200]);
     }
 }
